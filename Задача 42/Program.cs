6fd0e6fc89f45ef9e2eec80b73060b1c2047b573
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-05 06:21:32 +0300

    Added ex.42

---
+++ Задача 42/Program.cs
@@ -0,0 +1,29 @@
+﻿string buf1;
+Console.WriteLine("Ведите возраст Тани:");
+buf1 = Console.ReadLine();
+double x = Convert.ToDouble(buf1);
+
+string buf2;
+Console.WriteLine( "Введите возраст Мити:");
+buf2 = Console.ReadLine();
+double y = Convert.ToDouble(buf2);
+
+double avr = (x + y)/2;
+Console.WriteLine( "Средний возраст равен: {0}", avr);
+
+double a = avr - x;
+if(a<=0)
+{
+    a = a * (-1);
+}
+
+double b = a;
+
+     
+Console.WriteLine("Возраст Тани отличается от среднего значения на {0} лет Возраст Мити  отличается от среднего значения на {1} лет", a, b);
+
+
+
+
+ 
+    
