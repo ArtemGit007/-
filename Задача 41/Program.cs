cc1ef6a28aef8c4d48b812fc6201b959efacab18
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-05 05:46:00 +0300

    Added ex.41

---
+++ Задача 41/Program.cs
@@ -0,0 +1,16 @@
+﻿ // Найти сумму членов арифметической прогрессии, если известны ее первый член, 
+// знаменатель и число членов прогрессии.
+
+Console.WriteLine("Ведите первое чиcло мат. прогрессии:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine( "Введите количество членов прогрессии:");
+int numbers = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите разность прогресии:");
+int d = Convert.ToInt32(Console.ReadLine()); 
+
+// Формула вычисления суммы через первое число, количество чисел прогрессии и 
+// дельту.   S = (2a + d(numbers - 1))/2 * numbers.
+
+int sum = ((2 * a) + d*(numbers - 1))/2 * numbers;
+
+Console.WriteLine(sum);
